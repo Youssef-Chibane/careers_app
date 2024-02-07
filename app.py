aae057a37e3ad207cddc1dee75a00423d9e53ec7
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-02-07 22:41:59 +0100

    fake-data

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS= [
+    {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Florida, USA',
+    'salary': '$140,000',
+    },
+    {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Remote',
+    'salary': '$130,000',
+    },
+    {
+    'id': 3,
+    'title': 'Frontend',
+    'location': 'Remote',
+    'salary': '$100,000',
+    },
+    {
+    'id': 4,
+    'title': 'Backend',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000',
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
