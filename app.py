8c9568707819ef68c623310e2545eb303a15eee3
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-02-11 22:57:14 +0100

    api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -34,6 +34,10 @@ JOBS= [
 @app.route("/")
 def home():
     return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
