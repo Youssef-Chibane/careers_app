92a0378b5ebc8c5c30231328140f274d380ebc55
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-02-25 16:25:28 +0100

    added-database

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/joviancareers")
+engine = create_engine("mysql+pymysql://chibane:0516@172.19.176.187/careers?charset=utf8mb4")
 
-with engine.connect() as conn:
-    result = conn.execute(text("SHOW DATABASES"))
-    print(result.all())
+def load_jobs():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        row = result.all()
+        return row
+    
