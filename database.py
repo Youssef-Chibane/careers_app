459dbc0532d9d789ba3190fe3c3a07c3f2f7ba82
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-03-03 22:35:02 +0100

    fixed-function

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 from sqlalchemy import create_engine, text
 
 engine = create_engine("mysql+pymysql://chibane:0516@172.19.176.187/careers?charset=utf8mb4")
+
 
 def load_jobs():
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs"))
         row = result.all()
         return row
-    
