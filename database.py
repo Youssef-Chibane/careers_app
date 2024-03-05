ea6999553af0b34b5f503e2c6615da951583827c
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-03-05 21:10:42 +0100

    fix-load_jobs

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from sqlalchemy import create_engine, text
+import os
 
 engine = create_engine("mysql+pymysql://chibane:0516@172.19.176.187/careers?charset=utf8mb4")
 
-
 def load_jobs():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
-        row = result.all()
-        return row
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
+
