f9396d12daff2c1451d174c69b28d62d470fca42
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-03-14 23:09:37 +0000

    job by id function

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 
 engine = create_engine("mysql+pymysql://chibane:0516@172.19.176.187/careers?charset=utf8mb4")
 
-def load_jobs():
+def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
@@ -13,3 +13,11 @@ def load_jobs():
       jobs.append(dict(row._mapping))
     return jobs
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    row = result.fetchone()
+    if row:
+      return row._asdict()
+    else:
+      return None
